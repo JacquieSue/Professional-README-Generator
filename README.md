8a443589f23d76d811163d704a0ee48f78a2891b
JacquieSue <112510142+JacquieSue@users.noreply.github.com>
2022-11-15 10:40:03 -0700

    modified gitignore, README, and index, js files

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # Professional-README-Generator
-Node.js Challenge
+## Node.js Challenge 
+<!-- // TODO: Include packages needed for this application -->
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const generateREADME = ({ Description, TableOfContents, Installation, Usage, License, Contributing, Tests, Questions, email, GitHub }) =>
+  <!-- // TODO: Create an array of questions for user input -->
+  <!-- //  const questions = ['What is your GitHub username?','What is your GitHub username?']; -->
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'GitHub',
+        message: 'What is your GitHub username?',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email URL?',
+      },
+    ])
+    .then((answers) => {
+      const htmlPageContent = generateREADME(answers);
+      <!-- // TODO: Create a function to write README file -->
+      <!-- // function writeToFile(fileName, data) {} -->
+      fs.writeFile('README.md', htmlPageContent, (error) =>
+        error ? console.log(error) : console.log('Successfully created README.md!')
+      )
+    })
+<!-- // TODO: Create a function to initialize app -->
+function init() { }
+
+<!-- // Function call to initialize app -->
+init();
+
--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 // TODO: Include packages needed for this application
-// const inquirer = require('inquirer');
-// const fs = require('fs');
- 
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 const generateREADME = ({ Description, TableOfContents, Installation, Usage, License, Contributing, Tests, Questions, email, GitHub }) =>
-// TODO: Create an array of questions for user input
-// const questions = [:My Git];
-inquirer
-.prompt([
-    {
+  // TODO: Create an array of questions for user input
+   const questions = ['What is your GitHub username?','What is your GitHub username?'];
+  
+   inquirer
+    .prompt([
+      {
         type: 'input',
         name: 'GitHub',
         message: 'What is your GitHub username?',
@@ -15,18 +16,19 @@ inquirer
       {
         type: 'input',
         name: 'email',
-        message: 'Enter your email URL.',
+        message: 'What is your email URL?',
       },
-])
-.then((answers) => {
-const htmlPageContent = generateREADME(answers);
-// TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
-fs.writeFile('README.md', htmlPageContent, (error) =>
-error ? console.log(error) : console.log('Successfully created README.md!')
-)})
+    ])
+    .then((answers) => {
+      const htmlPageContent = generateREADME(answers);
+      // TODO: Create a function to write README file
+      // function writeToFile(fileName, data) {}
+      fs.writeFile('README.md', htmlPageContent, (error) =>
+        error ? console.log(error) : console.log('Successfully created README.md!')
+      )
+    })
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
-init();
+init ()
