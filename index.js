984f73d79218381f1007b96ad053d6d984a39367
JacquieSue <112510142+JacquieSue@users.noreply.github.com>
2022-12-03 12:56:47 -0700

    updated generateMarkdown file, and index.js file, added recording to usage section in generated README file

--- index.js
+++ index.js
@@ -4,87 +4,63 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
-
- inquirer
- .prompt([
-  {
-    type: 'input',
-    name: 'GitHub',
-    message: 'What is your GitHub username?',
-  },
-  {
-    type: 'input',
-    name: 'email',
-    message: 'What is your email URL?',
-  },
-  {
-    type: 'input',
-    name: 'titile',
-    message: 'What is the title of your project?',
-  },
-  {
-    type: 'input',
-    name: 'description',
-    message: 'Write a description of your project.',
-  },
-  {
-    type: 'input',
-    name: 'installation',
-    message: 'If applicable, describe the steps required to install your project.',
-  },
-  {
-    type: 'input',
-    name: 'usage',
-    message: 'Provide instructions and examples of your project.',
-  },
-  {
-    type: 'input',
-    name: 'contributing',
-    message: 'I applicable, provide guidelines on how other developers can contribute to your project.',
-  },
-  {
-    type: 'input',
-    name: 'tests',
-    message: 'If applicable, provide any tests written for your application and provide examples on how to run them.?',
-  },
-  {
-    type: 'input',
-    name: 'table of contents',
-    message: 'Here is the Table of Contents', 
-  // - [Installation](#installation)
-  // - [Usage](#usage)
-  // - [Credits](#credits)
-  // - [License](#license)
-  // - [Description](#description)
-  // - [Tests](#tests)
-  // - [Questions](#questions)
-  // - [Contact - me](#contact - me)]
-  },
-{
-  type: 'input',
-    name: 'email',
-      message: 'What is your email URL?',  
-  },
-{
-  type: 'input',
-    name: 'email',
-      message: 'What is your email URL?',  
-  },
-])
-
-// TODO: Create a function to initialize app
-// init()
-// questions()
-  .then((answers) => {
-    const READMEContent = generateMarkdown;// writeFile('README.md', generateMarkdown({ ...answers })))
-  });
+function init() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Write a description of your project.',
+      },
+      {
+        type: 'input',
+        name: 'tableOfContents',
+        message: 'Here is the Table of Contents',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'If applicable, describe the steps required to install your project.',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples of your project.',
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'List the project contributors',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'If applicable, provide any tests written for your application and provide examples on how to run them.?',
+      },
+      {
+        type: 'input',
+        name: 'user',
+        message: 'What is your GitHub username?',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email URL?',
+      },
+    ])
+    .then((param) => {
+      writeToFile(param);
+    })
+}
 // // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {
-  fs.writeFile('README.md', READMEContent, (error) => 
-    error ? console.log(error) : console.log('Successfully created README.md!')
-  )
-  
-  
-
+function writeToFile(answers) {
+  fs.writeFile('README.md', generateMarkdown(answers), (error) =>
+    error ? console.log(error) : console.log('Successfully created README.md!'))
+}
 // Function call to initialize app
-// init();
+init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,28 +1,52 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if(data.license = true);
+  if (data.license.badge = true);
 }
 
-  // TODO: Create a function that returns the license link
+// TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title};
-  ## ${data.description};
-  ## ${data.license};
-  ## ${data.installation};
-  ## ${data.usage};
-  ## ${data.contributing};
-  ## ${data.tests};
-  ## ${data.questions};
-  ## ${data.contact-me};
+function generateMarkdown(answers) {
+  return `
+  # Title
+${answers.title}
+
+## Description
+${answers.description}
+
+## Table Of Contents
+* [Description](#description)
+* [Installation](#installation)
+* [Tests](#tests)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [License](#license)
+* [Questions](#questions)
+
+
+
+## Installation
+${answers.installation}
+
+## Usage
+![Recording]()
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+## Questions
+${answers.user}
+${answers.email}
   `;
 }
 
